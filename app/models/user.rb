6d4d41eb47e12ae4f1d1efd4b3e876dd10b17189
enju-pectin <enju.pectin0802@gmail.com>
2020-01-23 23:42:48 +0900

    ユーザの新規登録フォーム・バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :first_name, :last_name, :firstname_kana, :lastname_kana, :birthdate , presence: true
 end
