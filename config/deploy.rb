b116dd07fcbdbf98d95377640afa28319fa07ca0
shwe-yuji <okumura19943210yuji@gmail.com>
2020-01-26 17:37:43 +0900

    secrets.ymlをcredentials.yml.encに変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,13 +46,13 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
     end
   end
   before :starting, 'deploy:upload'
