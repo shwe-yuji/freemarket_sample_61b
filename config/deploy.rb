093fe0c3bba95d12f5fe446737abe7228fb60023
shwe-yuji <okumura19943210yuji@gmail.com>
2020-01-26 12:02:35 +0900

    basic認証自動デプロイの設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,3 +32,10 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+
+set :default_env, {
+  BASIC_AUTH_USER:  ENV["BASIC_AUTH_USER"]
+  BASIC_AUTH_PASSWORD:  ENV["BASIC_AUTH_PASSWORD"]
+#   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  }
