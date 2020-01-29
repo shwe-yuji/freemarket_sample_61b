e8b8dffde0149fc7d0742120cbe6603254959b66
shwe-yuji <okumura19943210yuji@gmail.com>
2020-01-29 14:33:02 +0900

    carierwave.rbをリファクタリング

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,9 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if Rails.env.development? || Rails.nev.test?
+    config.storage :file
+  elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -15,8 +17,5 @@ CarrierWave.configure do |config|
 
     config.fog_directory  = 'freemarket-sample-61b'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-61b'
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
   end
 end
