63991cb3bd0573042fd98b32afa6b2a9661907fd
shwe-yuji <okumura19943210yuji@gmail.com>
2020-02-18 04:16:41 +0900

    ルーティング、コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products, only: :new
   get 'home/top'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit] do
+    resources :credit_cards, only: [:show]
+  end
 end
