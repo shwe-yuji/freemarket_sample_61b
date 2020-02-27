24aefec2e73cd3a2f6cf3879bcc594162d985530
shwe-yuji <okumura19943210yuji@gmail.com>
2020-02-28 03:26:51 +0900

    画像表示完了

--- app/assets/javascripts/products_images.js
+++ app/assets/javascripts/products_images.js
@@ -1,29 +1,57 @@
-$(document).on('turbolinks:load', ()=> {
-  // 画像用のinputを生成する関数
-  const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="product[images_attributes][${index}][src]"
-                    id="product_images_attributes_${index}_src"><br>
-                    <div class="js-remove">削除</div>
-                  </div>`;
-    return html;
+$(document).on('turbolinks:load', function(){
+  const buildInputArea = function(){
+    var html = `<div class="sell-area__dropbox__another">
+                  <label class="sell-area__dropbox__another__img">
+                    <i class="fa fa-camera camera-icon-2"></i>
+                    <p class="sell-area__dropbox__img--text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード</p>
+                    <input type="file" name="product[photos_attributes][0][photo]" id="product_photos_attributes_0_photo">
+                  </label>
+                </div>`
+  return html;
   }
+  
+  var image_ctn = 0;
 
-  // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  $('.sell-area__dropbox').on('change', 'input[type="file"]', function(e){
+    e.preventDefault();
+    var file = $(this).prop('files')[0];
+    var reader = new FileReader();
 
-  $('#image-box').on('change', '.js-file', function(e) {
-    // fileIndexの先頭の数字を使ってinputを作る
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    // 末尾の数に1足した数を追加する
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+
+    reader.onload = function(){
+      var src = reader.result
+      var preview = `<div class="preview">
+                    <div class="preview__img">
+                      <img src="${src}" width="100" height="100">
+                    </div>
+                    <div class="preview__link">
+                      <a href="#">編集する</a>
+                      <a href="#">削除する</a>
+                    </div>
+                 </div>`
+      image_ctn += 1;
+      console.log(image_ctn);
+      if (image_ctn < 6) {
+        $('.sell-area__dropbox').prepend(preview);
+        $('.sell-area__dropbox__img--text').remove();
+        if (image_ctn == 5) {
+          $('.sell-area__dropbox').append(buildInputArea);
+          $('i.fa.fa-camera.camera-icon-1').remove();
+        }
+      } else if (image_ctn > 5) {
+        $('.sell-area__dropbox__another').prepend(preview);
+        $('.sell-area__dropbox__img--text').remove();
+        if (image_ctn == 10) {
+          $('i.fa.fa-camera.camera-icon-2').remove();
+        }
+      } else if (image_ctn > 11) {
+          alert('これ以上は画像を投稿できません。');
+      } 
+    }
+    reader.readAsDataURL(file);
   });
-
-  $('#image-box').on('click', '.js-remove', function() {
-    $(this).parent().remove();
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  });
-});
+})
