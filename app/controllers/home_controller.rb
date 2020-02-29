cf12fcc45f0bda25273088e3ea482dd3494f3d76
ruba0618 <ruba061806180618@gmail.com>
2020-02-29 20:03:53 +0900

    商品購入完了ページのフロント実装
    What
    商品購入完了ページのフロントを実装。
    購入完了のお知らせ、出品者の評価を機能させる。
    
    Why
    本アプリケーションのユーザーの購入時のゴールにあたるため。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
 
   def show
   end
+
+  def done
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :products, only: :new
   get 'home/top'
+  get 'home/done'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :home, only: [:show]
   resources :users, only: [:show, :edit]
+  
 end
