f695f824e2dcf1ea6f9b274ae93d656e5695a1fb
@shikou0918 <shikou.0918@gmail.com>
2020-02-29 17:03:21 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_113541) do
+ActiveRecord::Schema.define(version: 2020_02_28_142642) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+    t.index ["name"], name: "index_categories_on_name"
   end
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -29,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_113541) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
-    t.integer "category_id", null: false
     t.integer "size_id", null: false
     t.integer "status_id", default: 1, null: false
     t.integer "condition_id", null: false
@@ -41,10 +49,13 @@ ActiveRecord::Schema.define(version: 2020_02_25_113541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "brand_id"
+    t.bigint "category_id", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["name"], name: "index_products_on_name"
   end
 
   add_foreign_key "photos", "products"
   add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
 end
