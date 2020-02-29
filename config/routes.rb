1e4640b882eb8e29aa60b5a1459ef9fbab7d02b7
@shikou0918 <shikou.0918@gmail.com>
2020-02-29 17:38:41 +0900

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products, except: :show
   get 'home/top'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit] do
+    resources :credit_cards, only: [:show]
+  end
+  resources :home, only: [:show]
 end
