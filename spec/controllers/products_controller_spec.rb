4e84fcc051e710fb798dc28f7343a0378fbc616b
@shikou0918 <shikou.0918@gmail.com>
2020-03-07 15:33:22 +0900

    products_controller.rbの編集

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe ProductsController, type: :controller do
+  describe 'Delete #destroy' do
+      it "商品削除後、トップ画面に遷移" do
+        get :index
+        expect(response).to render_template :index
+      end
+
+      it "商品の削除" do
+        product = create(:product)
+        expect{
+          delete :destroy, params: { id: product }
+        }.to change(Product,:count).by(-1)
+      end
+  end
+end
