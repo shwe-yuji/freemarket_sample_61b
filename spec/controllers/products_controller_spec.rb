57667631b23e4845f91dde5d4c9949e02e96b06e
@shikou0918 <shikou.0918@gmail.com>
2020-03-07 16:46:45 +0900

    高田さんからの指摘事項の修正

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 describe ProductsController, type: :controller do
   describe 'Delete #destroy' do
-    it "商品削除後、トップ画面に遷移" do
-      get :index
-      expect(response).to render_template :index
-    end
-
     it "商品の削除" do
       product = create(:product)
       expect{
