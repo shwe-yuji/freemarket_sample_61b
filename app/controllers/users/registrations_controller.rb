604e877f66e17a0da62867c7ad8b3fe1c8e1ce92
shwe-yuji <okumura19943210yuji@gmail.com>
2020-03-15 14:38:06 +0900

    メンバーコードレビュー後の訂正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -26,18 +26,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def step2_regist
-    input_phone_number = params[:telephone].sub(/\A./,'+81').gsub(/-/,"")
-    sms_num = rand(10000..99999)
-    session[:sms_num] = sms_num
-    client = Twilio::REST::Client.new(config.account_sid, config.auth_token)
-    begin
-      client.messages.create(
-        from: Rails.application.credentials[:TWILIO_NUMBER],
-        to: input_phone_number,
-        body: "#{sms_num}を入力してください"
-      )
-    rescue Twilio::REST::RestError => e
-    end
+    # input_phone_number = params[:telephone].sub(/\A./,'+81').gsub(/-/,"")
+    # sms_num = rand(10000..99999)
+    # session[:sms_num] = sms_num
+    # client = Twilio::REST::Client.new(config.account_sid, config.auth_token)
+    # begin
+    #   client.messages.create(
+    #     from: Rails.application.credentials[:TWILIO_NUMBER],
+    #     to: input_phone_number,
+    #     body: "#{sms_num}を入力してください"
+    #   )
+    # rescue Twilio::REST::RestError => e
+    # end
     redirect_to phone_confirm_path
   end
 
@@ -45,8 +45,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def phone_confirm_input
-    input_sms_number = params[:input_sms_number].to_i
-    if session[:sms_num] === input_sms_number
+    # input_sms_number = params[:input_sms_number].to_i
+    # if session[:sms_num] === input_sms_number
       redirect_to destination_regist_path
     # else
     #   redirect_to phone_regist_path
