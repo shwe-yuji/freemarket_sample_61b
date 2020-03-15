9da6e832c344d4fbc4d8f94a12af2f3819f08502
shwe-yuji <okumura19943210yuji@gmail.com>
2020-03-16 00:45:30 +0900

    パンくず下層を定義

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,13 +2,19 @@ crumb :root do
   link "メルカリ", root_path
 end
 
-crumb :categories do
+crumb :category_all do
   link "カテゴリー一覧", categories_path
+  parent :root
 end
 
-crumb :category do |category|
+crumb :parent_categories do |category|
   link category.name, category_path
-  parent :categories
+  parent :category_all
+end
+
+crumb :child_categories do |category|
+  link category.name, categories_path
+  parent :parent_categories
 end
 
 # crumb :project do |project|
