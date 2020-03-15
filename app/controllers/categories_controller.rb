c79e68cf5b226dbfb96c029ae9d25d01fce0c5eb
@shikou0918 <shikou.0918@gmail.com>
2020-03-15 17:43:41 +0900

    商品一覧の表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,12 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    # @categoryが子カテゴリの場合,@productsは子カテゴリが入る
+    if @category.has_children?
+      @products = Product.where(category_id: @category.children).order('created_at DESC')
+    # @categoryが子カテゴリではない場合(孫カテゴリの場合),@productsは孫カテゴリが入る
+    else @category.childless?
+      @products = Product.where(category_id: @category.path_ids).order('created_at DESC')
+    end
   end
 end
