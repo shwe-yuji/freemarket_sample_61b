c01ede468f97d6f8093a1e9082c6282cdd301178
shwe-yuji <okumura19943210yuji@gmail.com>
2020-03-19 04:55:29 +0900

    パンくずカテゴリ完成？

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,18 +1,31 @@
+#ホーム画面へ遷移
 crumb :root do
   link "メルカリ", root_path
 end
 
+#カテゴリー一覧へ遷移
 crumb :category_all do
   link "カテゴリー一覧", categories_path
   parent :root
 end
 
+#親カテゴリーが選択された場合
 crumb :parent_categories do |category|
-  link category.name, category_path
+  link category, category_path
   parent :category_all
 end
 
+#子カテゴリーが選択された場合
 crumb :child_categories do |category|
-  link category.name, category_path
-  parent :parent_categories
+  link category.parent.name, category_path(category.parent.id)
+  link category.name, category_path(category.id)
+  parent :category_all
+end
+
+#孫カテゴリーが選択された場合 
+crumb :grand_child_categories do |category|
+  link category.parent.parent.name, category_path(category.parent.parent.id)
+  link category.parent.name, category_path(category.parent.id)
+  link category.name, category_path(category.id)
+  parent :category_all
 end
