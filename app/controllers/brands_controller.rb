7f7a6f5f136ea7675d2049d153a0d5a07ad634db
Monma_Takeshi <jemini0527@gmail.com>
2020-03-18 22:10:33 +0900

    brands_controller group_showのビューへ渡すインスタンス変数の定義
    brands_controllerでgroup_showのビューへ渡すインスタンス変数を定義しました。
    
    @category_root → カテゴリテーブルのルート階層のレコードすべてを代入
    @current_category → 当該ページIDのカテゴリのレコードを代入
    @current_brands → 当該ページのカテゴリに含まれるブランドのレコードをすべて代入

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,8 @@
 class BrandsController < ApplicationController
+  def group_show
+    @category_root = Category.where(ancestry: nil)
+    @current_category = Category.find(params[:id])
+    current_brand_ids = Product.where(id: @current_category.subtree.ids).pluck(:brand_id)
+    @current_brands = Brand.group('name').order('name').where(id: current_brand_ids)
+  end
 end
