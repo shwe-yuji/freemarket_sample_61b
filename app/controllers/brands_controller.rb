812dde808a7cc91153db715750e061666e87a51e
Monma_Takeshi <jemini0527@gmail.com>
2020-03-18 22:30:34 +0900

    brands_controller showアクションのビューへ渡すインスタンス変数の定義
    brands_controllerでshowアクションのビューへ渡すインスタンス変数を定義しました。
    
    @current_brand → 当該ページIDのブランドのレコードを代入
    @current_brand_products → 当該ページIDのブランドを持っている商品のレコードをすべて代入
    @categories_in_brand → 当該ページのブランドを持っているカテゴリーのレコードをすべて代入

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,4 +5,11 @@ class BrandsController < ApplicationController
     current_brand_ids = Product.where(id: @current_category.subtree.ids).pluck(:brand_id)
     @current_brands = Brand.group('name').order('name').where(id: current_brand_ids)
   end
+
+  def show
+    @current_brand = Brand.find(params[:id])
+    @current_brand_products = Product.where(brand_id: @current_brand.id)
+    categories_in_brand_ids = @current_brand_products.pluck(:category_id)
+    @categories_in_brand = Category.find(categories_in_brand_ids)
+  end
 end
