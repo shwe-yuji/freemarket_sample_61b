767587dafa4c13fa668a11c82b754b0146cc0db7
Monma_Takeshi <jemini0527@gmail.com>
2020-03-19 01:17:06 +0900

    brands_controller categoryアクションのビューへ渡すインスタンス変数の定義
    brands_controllerでcategoryアクションのビューへ渡すインスタンス変数を定義しました。
    また、@current_categoryはgroup_showアクションと共通化できたので別メソッドにしてbefore_actionで読み込むようにしました。
    
    @current_brand → 当該ページのbrand_idのブランドのレコードを代入
    @products_in_brand_category → 当該ページIDのブランドとカテゴリーを持っている商品のレコードをすべて代入

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,8 +1,8 @@
 class BrandsController < ApplicationController
-  
+  before_action :set_current_category, only: [:group_show, :category]
+
   def group_show
     @category_root = Category.where(ancestry: nil)
-    @current_category = Category.find(params[:id])
     current_brand_ids = Product.where(id: @current_category.subtree.ids).pluck(:brand_id)
     @current_brands = Brand.group('name').order('name').where(id: current_brand_ids)
   end
@@ -12,6 +12,17 @@ class BrandsController < ApplicationController
     @current_brand_products = Product.where(brand_id: @current_brand.id)
     categories_in_brand_ids = @current_brand_products.pluck(:category_id)
     @categories_in_brand = Category.find(categories_in_brand_ids)
+  end
+
+  def category
+    @current_brand = Brand.find(params[:brand_id])
+    @products_in_brand_category = Product.where(brand_id: @current_brand, category_id: @current_category)
+  end
+
+  private
+
+  def set_current_category
+    @current_category = Category.find(params[:id])
   end
 
 end
