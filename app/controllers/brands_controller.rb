ede985d1af014537fe517ffeacfaee1c4d57f387
Monma_Takeshi <jemini0527@gmail.com>
2020-03-20 20:05:49 +0900

    pagy導入 brands_controller.rb
    pagyの導入。
    コントローラーでinclude Pagy::Backendで読み込みを行い、各アクションでは@pagyを定義しました。

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,4 +1,5 @@
 class BrandsController < ApplicationController
+  include Pagy::Backend
   before_action :set_current_category, only: [:group_show, :category]
 
   def group_show
@@ -9,14 +10,15 @@ class BrandsController < ApplicationController
 
   def show
     @current_brand = Brand.find(params[:id])
-    @current_brand_products = Product.where(brand_id: @current_brand.id)
+    @pagy, @current_brand_products = pagy(Product.where(brand_id: @current_brand.id))
     categories_in_brand_ids = @current_brand_products.pluck(:category_id)
     @categories_in_brand = Category.find(categories_in_brand_ids)
   end
 
   def category
-    @current_brand = Brand.find(params[:brand_id])
-    @products_in_brand_category = Product.where(brand_id: @current_brand, category_id: @current_category)
+    current_brand = Brand.find(params[:brand_id])
+    @pagy, @products_in_brand_category = pagy(Product.where(brand_id: current_brand, category_id: @current_category))
+    @brand_category_name = @current_brand.name + " " + @current_category.name
   end
 
   private
@@ -24,5 +26,4 @@ class BrandsController < ApplicationController
   def set_current_category
     @current_category = Category.find(params[:id])
   end
-
 end
