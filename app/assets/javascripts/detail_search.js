f61f6b123750bc775597d5a57127b6eb20a6a6c7
shwe-yuji <okumura19943210yuji@gmail.com>
2020-03-21 20:48:40 +0900

    検索フォームのjsファイル作成

---
+++ app/assets/javascripts/detail_search.js
@@ -0,0 +1,27 @@
+// $(document).on('turbolinks:load', function(){ 
+//   $('.detail-search__box').on('click', '#condition_all', function(){
+//     $('#condition_id_1').prop('checked', true);
+//     $('#condition_id_2').prop('checked', true);
+//     $('#condition_id_3').prop('checked', true);
+//     $('#condition_id_4').prop('checked', true);
+//     $('#condition_id_5').prop('checked', true);
+//     $('#condition_id_6').prop('checked', true);
+//     $('#condition_id_7').prop('checked', true);
+//   })
+
+//   $('.detail-search__box').on('click', '#delivery_expence_all', function(){
+//     $('#delivery_expence_id_1').prop('checked', true);
+//     $('#delivery_expence_id_2').prop('checked', true);
+//   })
+
+//   $('.detail-search__box').on('click', '#status_all', function(){
+//     $('#status_id_1').prop('checked', true);
+//     $('#status_id_2').prop('checked', true);
+//     $('#status_id_3').prop('checked', true);
+//     $('#status_id_4').prop('checked', true);
+//   })
+
+//   $('.detail-search__box').on('click', '.detail-search__box__form__submit-area--reset', function(){
+//     return false;
+//   })
+// })
