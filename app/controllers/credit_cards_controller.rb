a54990d244d7e5122738775929b6c55510b9dca5
enju-pectin <enju.pectin0802@gmail.com>
2020-03-21 20:24:35 +0900

    Update credit_cards_controller.rb

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,7 +2,7 @@ class CreditCardsController < ApplicationController
   require 'payjp'
   before_action :set_card
 
-  def buy #クレジット購入
+  def buy
     if @card.blank?
       redirect_to action: "new"
       flash[:alert] = '購入にはクレジットカード登録が必要です'
@@ -14,9 +14,9 @@ class CreditCardsController < ApplicationController
         Payjp.api_key = Rails.application.credentials[:PAYJP_SECRET_KEY]
       end
       Payjp::Charge.create(
-      amount: @product.price, #支払金額
-      customer: @card.customer_id, #顧客ID
-      currency: 'jpy', #日本円
+      amount: @product.price,
+      customer: @card.customer_id,
+      currency: 'jpy',
       )
      @transaction = TransactionRecord.new(product_id: params[:id], user_id: current_user.id) 
 
@@ -29,42 +29,6 @@ class CreditCardsController < ApplicationController
         redirect_to :root
       end
     end
-  end
-
-
-  # クレジットカード情報入力画面
-  # def new
-  #   if @card
-  #     redirect_to card_path unless @card
-  #   else
-  #     render 'mypages/create_card'
-  #   end
-  # end
-
-  # 登録画面で入力した情報をDBに保存
-  # def create
-  #   Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-  #   if params['payjp-token'].blank?
-  #     render 'mypages/create_card'
-  #   else
-  #     customer = Payjp::Customer.create( # ここで先ほど生成したトークンを顧客情報と紐付け、PAY.JP管理サイトに送信
-  #       email: current_user.email,
-  #       card: params['payjp-token'],
-  #       metadata: {user_id: current_user.id} # 記述しなくても大丈夫です
-  #     )
-  #     @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-  #     if @card.save
-  #       redirect_to cards_path
-  #     else
-  #       render 'mypages/create_card'
-  #     end
-  #   end
-  # end
-
-  def show
-  end
-
-  def destroy
   end
 
   private
