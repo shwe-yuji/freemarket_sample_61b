d0b269a4b054b405dfab2745b16f09732547af82
Monma_Takeshi <jemini0527@gmail.com>
2020-03-22 16:21:52 +0900

    sider指摘点の修正 #の後ろにスペース追加
    siderで#の後のスペースを入れるよう指摘があったため、実施しました。

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,28 +1,28 @@
-#ホーム画面へ遷移
+# ホーム画面へ遷移
 crumb :root do
   link "メルカリ", root_path
 end
 
-#カテゴリー一覧へ遷移
+# カテゴリー一覧へ遷移
 crumb :category_all do
   link "カテゴリー一覧", categories_path
   parent :root
 end
 
-#親カテゴリーが選択された場合
+# 親カテゴリーが選択された場合
 crumb :parent_categories do |category|
   link category, category_path
   parent :category_all
 end
 
-#子カテゴリーが選択された場合
+# 子カテゴリーが選択された場合
 crumb :child_categories do |category|
   link category.parent.value, category_path(category.parent.id)
   link category.value, category_path(category.id)
   parent :category_all
 end
 
-#孫カテゴリーが選択された場合 
+# 孫カテゴリーが選択された場合 
 crumb :grand_child_categories do |category|
   link category.parent.parent.value, category_path(category.parent.parent.id)
   link category.parent.value, category_path(category.parent.id)
@@ -30,7 +30,7 @@ crumb :grand_child_categories do |category|
   parent :category_all
 end
 
-#ブランド一覧へ遷移
+# ブランド一覧へ遷移
 crumb :brand_all do
   link "ブランド一覧", brand_group_path(1)
   parent :root
