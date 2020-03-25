694fff83cdfca5cfece168b9d7cdfbe7c79243eb
Monma_Takeshi <jemini0527@gmail.com>
2020-03-26 02:09:47 +0900

    reCAPTCHA sessions_controller.rb 処理を記載
    reCAPTCHAの機能実装。
    sessions_controllerにreCAPTCHA認証が起こった時、起こらない時の処理を記載しました。
    処理のメソッドはprivate配下に記載し、before_actionでcreateアクションの前に呼び出すようにしました。

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  prepend_before_action :check_captcha, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
+
+  private
+    def check_captcha
+      unless verify_recaptcha
+        self.resource = resource_class.new sign_in_params
+        respond_with_navigational(resource) { render :new }
+      end 
+    end
 
   # GET /resource/sign_in
   # def new
