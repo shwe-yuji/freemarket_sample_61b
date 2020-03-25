6d2a17aaf39a2b6f0a1dd766f02bbc188892562b
@shikou0918 <shikou.0918@gmail.com>
2020-03-25 21:41:16 +0900

    不要なファイルの削除

--- app/controllers/api/products_controller.rb
+++
@@ -1,8 +0,0 @@
-# class Api::ProductsController < ApplicationController
-#   def new
-#   end
-
-#   def delivery_method
-#     @delivery_methods = DeliveryMethod.where(expense: params[:expense])
-#   end
-# end
