6ab25b71034673409107757f97e0f7fdb5e73b8a
@shikou0918 <shikou.0918@gmail.com>
2020-03-28 19:05:23 +0900

    配送方法のモデル修正

--- app/models/delivery_method2.rb
+++ app/models/delivery_method2.rb
@@ -1,9 +1,9 @@
 class DeliveryMethod2 < ActiveHash::Base
   self.data = [
       # 着払い(購入者負担)を選択した時
-      { id: 10, value: '未定'},
-      { id: 11, value: 'クロネコヤマト'},
-      { id: 12, value: 'ゆうパック'},
-      { id: 13, value: 'ゆうメール'} 
+      { id: 1, value: '未定'},
+      { id: 6, value: 'クロネコヤマト'},
+      { id: 7, value: 'ゆうパック'},
+      { id: 3, value: 'ゆうメール'},
   ]
 end
