acab618347371efb3d0341ab6cb8b5442f3b7642
Monma_Takeshi <jemini0527@gmail.com>
2020-03-30 02:40:29 +0900

    パンくず users_controllerに変数を定義
    パンくずリストに送る変数をコントローラーに定義しました。
    変数を送る必要があるのはリスティングページだけなので、それぞれのページ名を同じ名前の変数に入れました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,19 @@ class UsersController < ApplicationController
   end
 
   def listing_sale
+    @title = "出品した商品 - 出品中"
     @products_sale = Product.where( user_id: current_user.id, status_id: 1)
     # ログインユーザーが出品している商品を全て取得する
   end
 
   def listing_trade
+    @title = "出品した商品 - 取引中"
     @products_trade = Product.where( user_id: current_user.id, status_id: [2, 3])
     # ログインユーザーが取引している商品を全て取得する（仮）
   end
 
   def listing_soldout
+    @title = "出品した商品 - 売却済"
     @products_soldout = Product.where( user_id: current_user.id, status_id: 4 )
     # ログインユーザーが売薬済の商品を全て取得する
   end
