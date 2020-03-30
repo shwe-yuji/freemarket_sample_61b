e6f2efbea8f9bc5762c60112d115d3c2943e5e12
Monma_Takeshi <jemini0527@gmail.com>
2020-03-30 22:38:21 +0900

    ページタイトル設定 categories_controller
    カテゴリ関連のページにページタイトルをつけるため、@titleの変数の中身が各ページのタイトルになるよう変数を定義しました。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,15 @@
 class CategoriesController < ApplicationController
   include Pagy::Backend
+  before_action :title_word
+
   def index
+    @title = "カテゴリー一覧" + @title_end + " " + @title_introduction
     @category_parent = Category.where(ancestry: nil)
   end
 
   def show
     @category = Category.find(params[:id])
+    @title = @category.value + "(" + @category.root.value + ")" + @title_introduction_other
     if @category.is_root?
       @products = Product.where(category_id: @category.descendant_ids).order('created_at DESC')
       @pagy, @category_products = pagy(Product.where(category_id: @category.descendant_ids))
