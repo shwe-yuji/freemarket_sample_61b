d0eb9f168f54450cf214fa6a328bd770e2507e2f
Monma_Takeshi <jemini0527@gmail.com>
2020-03-30 22:35:46 +0900

    ページタイトル設定 brands_controller
    ブランド関連のページにページタイトルをつけるため、@titleの変数の中身が各ページのタイトルになるよう変数を定義しました。

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,8 +1,10 @@
 class BrandsController < ApplicationController
   include Pagy::Backend
   before_action :set_current_category, only: [:group_show, :category]
+  before_action :title_word
 
   def group_show
+    @title = @current_category.value + @title_end + " " + @title_introduction
     @category_root = Category.where(ancestry: nil)
     current_brand_ids = Product.where(id: @current_category.subtree.ids).pluck(:brand_id)
     @current_brands = Brand.group('name').order('name').where(id: current_brand_ids)
@@ -10,6 +12,7 @@ class BrandsController < ApplicationController
 
   def show
     @current_brand = Brand.find(params[:id])
+    @title = @current_brand.name + @title_introduction_other
     @pagy, @current_brand_products = pagy(Product.where(brand_id: @current_brand.id))
     categories_in_brand_ids = @current_brand_products.pluck(:category_id)
     @categories_in_brand = Category.find(categories_in_brand_ids)
@@ -17,6 +20,7 @@ class BrandsController < ApplicationController
 
   def category
     @current_brand = Brand.find(params[:brand_id])
+    @title = @current_brand.name + " " + @current_category.value + @title_introduction_other
     @pagy, @products_in_brand_category = pagy(Product.where(brand_id: @current_brand, category_id: @current_category))
     @brand_category_name = @current_brand.name + " " + @current_category.value
   end
