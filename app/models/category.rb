8211d70798a750047dc8d49785ea25d7394c2b00
Monma_Takeshi <jemini0527@gmail.com>
2020-04-03 13:36:53 +0900

    ルートid取得ロジック作成 categoryモデル
    categoryモデルに「カテゴリidの配列を渡すとそれぞれをルートidにした配列を返す」ロジックを記載。
    products#indexアクションで使用しました。

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,4 +9,14 @@ class Category < ApplicationRecord
       Category.find(category_id).subtree_ids
     end
   end
+
+  def self.conversion_root_ids(category_ids)
+    refine_categories = Category.where(id: category_ids)
+    # category_idsをルートIDに変換
+    root_ids = []
+    refine_categories.each do |category|
+      root_ids << category.root_id
+    end
+    return root_ids
+  end
 end
